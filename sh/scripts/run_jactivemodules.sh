28bc6d2c18e15269ce6968177a9ce1b9a42b8a3f
EC2 Default User <ec2-user@ip-172-31-23-233.us-east-2.compute.internal>
2018-11-09 00:25:45 +0000

    view

--- sh/scripts/run_jactivemodules.sh
+++ sh/scripts/run_jactivemodules.sh
@@ -1,19 +1,19 @@
-base_folder=/home/hag007/bnet
+base_folder=/home/ec2-user/bnet
 networks_folder=$base_folder/networks
 datasets_folder=$base_folder/datasets
-dataset_folder=/home/hag007/bnet/datasets/user1541628615.2
+dataset_folder=/home/ec2-user/bnet/datasets/user1541684459.48
 data_folder=$dataset_folder/data
 cache_folder=$dataset_folder/cache
 output_folder=$dataset_folder/output
 is_greedy=True
-algo_dir=/home/hag007/repos/bnetworks_alg/jactivemodules
+algo_dir=/home/ec2-user/bnetworks_alg/jactivemodules
 
-echo /home/hag007/bnet/datasets/user1541628615.2/cache/deg_edger.tsv
+echo /home/ec2-user/bnet/datasets/user1541684459.48/cache/deg_edger.tsv
 
 
 java -jar $algo_dir/jactivemodules.jar \
                                  $networks_folder/dip.sif \
-                                 /home/hag007/bnet/datasets/user1541628615.2/cache/deg_edger.tsv \
+                                 /home/ec2-user/bnet/datasets/user1541684459.48/cache/deg_edger.tsv \
                                  $is_greedy \
-                                 /home/hag007/bnet/datasets/user1541628615.2/output/jactivemodules_greedy_results.txt
+                                 /home/ec2-user/bnet/datasets/user1541684459.48/output/jactivemodules_greedy_results.txt
 
--- sh/scripts/run_keypathwayminer.sh
+++ sh/scripts/run_keypathwayminer.sh
@@ -1,10 +1,10 @@
-base_folder=/home/hag007/bnet
+base_folder=/home/ec2-user/bnet
 network_folder=$base_folder/networks
 datasets_folder=$base_folder/datasets
 data_folder=$dataset_folder/data
 cache_folder=$dataset_folder/cache
 output_folder=$dataset_folder/output
-dataset_folder=/home/hag007/bnet/datasets/user1541623440.35
+dataset_folder=/home/ec2-user/bnet/datasets/user1541677281.14
 
 pwd
 
